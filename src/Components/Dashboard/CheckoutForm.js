37479deef2688d52a9ebd58ad2054b10d439286a
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-02-08 19:59:42 +0600

    pymentmathod

--- src/Components/Dashboard/CheckoutForm.js
+++ src/Components/Dashboard/CheckoutForm.js
@@ -1,10 +1,31 @@
 import { async } from '@firebase/util';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { json } from 'react-router';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ order }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const [cardError, setCardError] = useState('')
+    const [clientSecret, setClientSecret] = useState('');
+    const { price } = order;
+    useEffect(() => {
+        fetch('http://localhost:5000/cerate-payment-intent', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+                'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify({price})
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data?.clientSecret) {
+                    setClientSecret(data.clientSecret)
+                }
+            });
+    }, [price])
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -17,29 +38,48 @@ const CheckoutForm = () => {
             return;
         }
 
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+
+
+        setCardError(error?.message || '')
+        //  if(error){
+        //   setCardError(error.message)
+        //  }
+        //  else{
+        //     setCardError('');
+        //  }
     }
     return (
-        <form onSubmit={handleSubmit}>
-            <CardElement
-                options={{
-                    style: {
-                        base: {
-                            fontSize: '16px',
-                            color: '#424770',
-                            '::placeholder': {
-                                color: '#aab7c4',
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
                             },
                         },
-                        invalid: {
-                            color: '#9e2146',
-                        },
-                    },
-                }}
-            />
-            <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe}>
-                Pay
-            </button>
-        </form>
+                    }}
+                />
+                <button className='btn btn-success btn-sm mt-4' type="submit" disabled={!stripe || !clientSecret}>
+                    Pay
+                </button>
+            </form>
+
+            {
+                cardError && <p className='text-red-500'>{cardError}</p>
+            }
+        </>
     );
 };
 
--- src/Components/Dashboard/Payment.js
+++ src/Components/Dashboard/Payment.js
@@ -12,7 +12,7 @@ const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
 const Payment = () => {
     const { id } = useParams();
     const url = `http://localhost:5000/booking/${id}`;
-    const { data: Order, isLoading } = useQuery(['booking', id], () => fetch(url, {
+    const { data: order, isLoading } = useQuery(['booking', id], () => fetch(url, {
 
         method: 'GET',
         headers: {
@@ -27,15 +27,15 @@ const Payment = () => {
 
             <div className="card w-50 max-w-md bg-base-100 shadow-xl">
                 <div className="card-body">
-                    <p className='text-success font-semibold text-2xl'>Hello, {Order.customerName}</p>
-                    <h2 className="card-title">pay for Product name: {Order.product} </h2>
-                    <p className='font-bold text-black'>please pay:$ {Order.price}</p>
+                    <p className='text-success font-semibold text-2xl'>Hello, {order.customerName}</p>
+                    <h2 className="card-title">pay for Product name: {order.product} </h2>
+                    <p className='font-bold text-black'>please pay:$ {order.price}</p>
                 </div>
             </div>
             <div className="card flex-shrink-0 w-50 mt-10 max-w-md shadow-2xl bg-base-100">
                 <div className="card-body">
                     <Elements stripe={stripePromise}>
-                        <CheckoutForm />
+                        <CheckoutForm order={order}/>
                     </Elements>
 
                 </div>
